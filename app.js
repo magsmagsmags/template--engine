e0aef35c2bea02550daee8daf3c24eb1a61502d7
Maggie Kiefer <57575667+magsmagsmags@users.noreply.github.com>
2020-01-20 21:30:54 -0600

    classes updated in 4 js docs

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const mustache = require('mustache');
+// const mustache = require('mustache');
 
-const Employee = require("./lib/employee");
-const Engineer = require("./lib/engineer");
-const Intern = require("./lib/intern");
-const Manager = require("./lib/manager");
-
-const generateHTML = require("./generateHTML.js");
-const generate = require("./generateHTML");
+// const Employee = require("./lib/employee");
+// const Engineer = require("./lib/engineer");
+// const Intern = require("./lib/intern");
+// const Manager = require("./lib/manager");
 
 // This array fills in with employee data.
-const teamMembers = [];
+// const teamMembers = [];
 
 async function userInput(data) {
-    inquirer.prompt()[
+    inquirer.prompt(data)[
         // * name
         {
             type: "input",
             message: "What is the name of this employee?",
-            name: "Name"
+            name: "name"
         },
         // * id
         {
             type: "input",
             message: "What is the employee ID?",
-            name: "ID"
+            name: "id"
         },
         // title
         {
@@ -47,29 +44,26 @@ async function userInput(data) {
             choices: ["Manager", "Intern", "Engineer"]
         },
 
-        if (data.role === "Manager") {
-            {
-                type: "input",
-                message: "What is the manager's office number?",
-                name: "officeNum"
-            }
+        //conditional questions for engineer, manager, intern
+        {
+            type: "input",
+            message: "What is the manager's office number?",
+            name: "office",
+
         },
 
-        if (data.role === "Intern") {
-            {
-                type: "input",
-                message: "What school does this intern attend?",
-                name: "school"
-            }
+        {
+            type: "input",
+            message: "What school does this intern attend?",
+            name: "school",
+            when: (data) => data.role === "Intern"
         },
 
-
-        if (data.role === "Engineer") {
-            {
-                type: "input",
-                message: "What is the engineer's github username?",
-                name: "github"
-            }
+        {
+            type: "input",
+            message: "What is the engineer's github username?",
+            name: "github",
+            when: (data) => data.role === "Engineer"
         },
 
         {
@@ -77,70 +71,34 @@ async function userInput(data) {
             message: "Would you like to add another team member?",
             name: "addAnother",
             choices: ["yes", "no"]
-        },
-
-        // {
-        //     type: "input",
-        //     message: "What is the engineer's github username?",
-        //     name: "github",
-        //     when: (data) => data.role === "Engineer"
-        // },
-        // {
-        //     type: "input",
-        //     message: "What school does this intern attend?",
-        //     name: "school",
-        //     when: (data) => data.role === "Intern"
-        // },
-
-        // * name
-        // * id
-        // * title
-        // * getName()
-        // * getId()
-        // * getEmail()
-        // * getRole() // Returns 'Employee'
-
-        // if manager:
-        //   * officeNumber
-        // * getRole() // Overridden to return 'Manager'
-
-        // if engineer:
-        // * github  // GitHub username
-        // * getGithub()
-        // * getRole() // Overridden to return 'Engineer'
-
-        // if intern:
-        //  * school 
-        // * getSchool()
-        // * getRole() // Overridden to return 'Intern'
-
-        // generate 
+        }
     ]
 };
 
-userInput();
+userInput().then(); {
 
-if (data.addAnother === "yes") {
-    userInput();
+    if (data.addAnother === "yes") {
+        userInput();
+    };
+
+    if (data.addAnother === "no") {
+        fs.writeFile("team.html", generateHTML, function (err) {
+            console.log(err);
+            if (err) return console.log(err);
+            console.log(response);
+        });
+    }
 };
 
-if (data.addAnother === "no") {
-    fs.writeFile("team.html", generateHTML, function (err) {
-        console.log(err);
-        if (err) return console.log(err);
-        console.log(response);
-    });
-};
 
 
 
 
-
-var source = document.getElementById("entry-template").innerHTML;
-var template = Handlebars.compile(source);
-//Get the HTML result of evaluating a Handlebars template by executing the template with a context.
-var context = {
-    title: "My New Post",
-    body: "This is my first post!"
-};
-var html = template(context);
+// var source = document.getElementById("entry-template").innerHTML;
+// var template = Handlebars.compile(source);
+// //Get the HTML result of evaluating a Handlebars template by executing the template with a context.
+// var context = {
+//     title: "My New Post",
+//     body: "This is my first post!"
+// };
+// var html = template(context);
